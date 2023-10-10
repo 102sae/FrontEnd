50128d1a5e82c5cd589ffbb731e20651e605d8b2
jeongmin <102chimee@gmail.com>
2023-10-10 13:09:22 +0900

    style : 색상 팔레트, 기본 css 추가

---
+++ src/styles/color.js
@@ -0,0 +1,10 @@
+const palette = {
+    white: '#ffffff',
+    black: '#000000',
+    shinhan_blue: '#0046FF',
+    sol_backgroud : '#88BDE7',
+    sol_text:'#1D449B',
+    ray_blue:'#A3C7F7',
+    ray_yellow:'#FFE27A',
+    ray_beige:'#FFE8D4',
+}
