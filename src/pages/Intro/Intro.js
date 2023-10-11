f7333c83039843c2ce6e61800f366d2881793789
jeongmin <102chimee@gmail.com>
2023-10-11 21:39:04 +0900

    add : 프렌즈 선택 추가

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -8,7 +8,16 @@ import MenuBox from "../../componets/MenuBox";
 import { ReactComponent as SolHi } from "../../assets/images/sol_hi.svg";
 import { ReactComponent as SolSmile } from "../../assets/images/sol_smile.svg";
 import { ReactComponent as SolWhy } from "../../assets/images/sol_why.svg";
-
+import { ReactComponent as LayHi } from "../../assets/images/lay_hi.svg";
+import { ReactComponent as MollyHi } from "../../assets/images/molly_hi.svg";
+import Lay from "../../assets/images/lay_default.svg";
+import LaySmile from "../../assets/images/lay_smile.svg";
+import Molly from "../../assets/images/molly_default.svg";
+import MollySmile from "../../assets/images/molly_smile.svg";
+import NoticeBox from "../../componets/NoticeBox";
+import Layname from "../../assets/images/lay_name.svg";
+import Mollyname from "../../assets/images/molly_name.svg";
+import FriendSelectBox from "../../componets/FriendSelectBox";
 function Intro() {
   const introScenario = [
     {
@@ -18,6 +27,7 @@ function Intro() {
       dialog:
         "밤하늘의 북쪽에서 가장 밝게 빛나는 별, \n북극성에 우주여행작가 쏠이 살고 있었어요.\n 그날도 어느 때와 같이 글을 써 내려가고 있었습니다.\n 다녀왔던 별들은 전부 아름다웠지만,",
       name: "??",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
@@ -28,6 +38,7 @@ function Intro() {
       image: "",
       dialog: "10년후...",
       name: "??",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
@@ -38,6 +49,7 @@ function Intro() {
       image: "",
       dialog: "안녕? 난 쏠이야",
       name: "??",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
@@ -48,6 +60,7 @@ function Intro() {
       image: <SolHi />,
       dialog: "난 부자야",
       name: "쏠",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
@@ -59,6 +72,7 @@ function Intro() {
       dialog:
         "북극성에서 또 다른 내 친구들이 온대! 혹시 그 친구들도 지구에서 부자가 될 수 있도록 도와줄 수 있을까?",
       name: "쏠",
+      arrowColor: palette.sol_text,
       menu: {
         show: true,
         option: ["나한테 맡겨!", "내가 왜?"],
@@ -69,8 +83,9 @@ function Intro() {
       index: 5,
       nextIndex: 8,
       image: <SolSmile />,
-      dialog: "와! 고마워ㅎㅎ 그럼 내 친구들을 소개시켜줄게",
+      dialog: "와! 고마워ㅎㅎ 너라면 들어줄줄 알았어!",
       name: "쏠",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
@@ -81,6 +96,7 @@ function Intro() {
       image: <SolWhy />,
       dialog: "오잉? 내가 사람을 잘못봤나? 다시 한번 생각해봐~",
       name: "쏠",
+      arrowColor: palette.sol_text,
       menu: {
         show: true,
         option: ["알겠어!", "미안.."],
@@ -93,16 +109,40 @@ function Intro() {
       image: <SolHi />,
       dialog: "아쉽네~ 다음에 또 보자~",
       name: "쏠",
+      arrowColor: palette.sol_text,
       menu: {
         show: false,
       },
     },
     {
       index: 8,
-      nextIndex: "",
+      nextIndex: 9,
       image: <SolHi />,
-      dialog: "더 친해지고 싶은 친구를 골라줘~",
+      dialog: "내 친구들을 소개시켜줄게~",
       name: "쏠",
+      arrowColor: palette.sol_text,
+      menu: {
+        show: false,
+      },
+    },
+    {
+      index: 9,
+      nextIndex: 10,
+      image: <LayHi />,
+      dialog: "안녕! 나는 지구에서 제일 유명한 뮤지션이 되고 싶은 레이야~",
+      name: "레이",
+      arrowColor: palette.ray_blue,
+      menu: {
+        show: false,
+      },
+    },
+    {
+      index: 10,
+      nextIndex: 11,
+      image: <MollyHi />,
+      dialog: "안녕~ 나는 지구에서 제일 멋진 식물 카페를 운영하고 있는 몰리야!",
+      name: "몰리",
+      arrowColor: palette.molly_purple,
       menu: {
         show: false,
       },
@@ -110,12 +150,15 @@ function Intro() {
   ];
 
   const [showMenuBox, setShowMenuBox] = useState(false);
+  const [showDialogBox, setShowDialogBox] = useState(true);
   const [currentScenarioIndex, setCurrentScenarioIndex] = useState(0);
 
   //다음 대화로 넘기기
   const handleDialogBoxClick = () => {
     if (currentScenarioIndex < introScenario.length - 1) {
       setCurrentScenarioIndex(introScenario[currentScenarioIndex].nextIndex);
+    } else {
+      setShowDialogBox(false);
     }
     console.log(currentScenarioIndex);
   };
@@ -146,7 +189,7 @@ function Intro() {
   return (
     <div
       className={`${styles.root}  ${
-        introScenario[currentScenarioIndex].name === "쏠"
+        introScenario[currentScenarioIndex].name !== "??"
           ? styles.solBackground
           : ""
       } `}
@@ -168,30 +211,58 @@ function Intro() {
           <div
             className={styles.characterWrap}
             onClick={
-              introScenario[currentScenarioIndex + 1] &&
-              introScenario[currentScenarioIndex + 1].menu.show
-                ? handleDialogBoxClick
-                : null
+              introScenario[currentScenarioIndex] &&
+              introScenario[currentScenarioIndex].menu.show
+                ? null
+                : handleDialogBoxClick
             }
           >
-            {introScenario[currentScenarioIndex].image}
-            <DialogBox
-              dialog={introScenario[currentScenarioIndex].dialog}
-              name={introScenario[currentScenarioIndex].name}
-              backgroundColor={palette.main_dialog}
-              arrowColor={palette.sol_text}
-            />
+            {showDialogBox && (
+              <div>
+                <div className={styles.characterImage}>
+                  {introScenario[currentScenarioIndex].image}
+                </div>
+                <DialogBox
+                  dialog={introScenario[currentScenarioIndex].dialog}
+                  name={introScenario[currentScenarioIndex].name}
+                  backgroundColor={palette.main_dialog}
+                  arrowColor={introScenario[currentScenarioIndex].arrowColor}
+                />
+              </div>
+            )}
           </div>
         )}
-        {/* 사용자의 동의 구하기 */}
+
         {showMenuBox && (
           <MenuBox
             select={introScenario[currentScenarioIndex].menu}
             onOptionClick={handleMenuOptionClick}
           />
         )}
+
+        {
+          /* 마지막 시나리오 후 프렌즈 선택창 */
+          !showDialogBox && (
+            <div className={styles.wrap}>
+              <div className={styles.friendsWrap}>
+                <FriendSelectBox
+                  friendNameImage={Layname}
+                  friendImage={Lay}
+                  hoverFriendImage={LaySmile}
+                  category="개념파"
+                />
+                <FriendSelectBox
+                  friendNameImage={Mollyname}
+                  friendImage={Molly}
+                  hoverFriendImage={MollySmile}
+                  category="실전파"
+                />
+              </div>
+              <NoticeBox notice="누구를 선택할까?" />
+            </div>
+          )
+        }
       </div>
-      <div></div>
     </div>
   );
 }
--- src/styles/color.js
+++ src/styles/color.js
@@ -1,12 +1,13 @@
 const palette = {
-    white: '#ffffff',
-    black: '#000000',
-    shinhan_blue: '#0046FF',
-    main_dialog: '#EBF3FC',
-    sol_backgroud : '#88BDE7',
-    sol_text:'#1D449B',
-    ray_blue:'#A3C7F7',
-    ray_yellow:'#FFE27A',
-    ray_beige:'#FFE8D4',
-}
+  white: "#ffffff",
+  black: "#000000",
+  shinhan_blue: "#0046FF",
+  main_dialog: "#EBF3FC",
+  sol_backgroud: "#88BDE7",
+  sol_text: "#1D449B",
+  ray_blue: "#A3C7F7",
+  ray_yellow: "#FFE27A",
+  ray_beige: "#FFE8D4",
+  molly_purple: "#4A3A55",
+};
 export default palette;
