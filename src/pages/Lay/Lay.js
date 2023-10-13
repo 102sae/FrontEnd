480ef970550cff26d6ff8f3792341c2dfeb19afe
hahaharin <harin2950@naver.com>
2023-10-13 11:29:30 +0900

    style : 폰트 적용

--- src/pages/Lay/Lay.js
+++ src/pages/Lay/Lay.js
@@ -12,6 +12,7 @@ import LayThinking from "../../assets/images/Lay/lay_thinking.svg"
 import ProgressBar from "../../componets/ProgressBar";
 import axios from 'axios';
 import Quiz from "../../componets/Quiz";
+import ReactTyped from "react-typed";
 
 function Lay() {
     const [showMenuBox, setShowMenuBox] = useState(false);
@@ -147,11 +148,13 @@ function Lay() {
                     </div>
                     {/* 대화 상자 렌더링 */}
                     <DialogBox
-                        dialog={LayScenario[currentScenarioIndex].dialog}
                         name="레이"
                         backgroundColor={palette.main_dialog}
                         arrowColor={palette.ray_blue}
                     />
+                    <div className={styles.dialogText}>
+                        <ReactTyped key={currentScenarioIndex} strings={[LayScenario[currentScenarioIndex].dialog]} typeSpeed={100} />
+                    </div>
                 </div>
                 )}
             </div>
@@ -166,17 +169,17 @@ function Lay() {
             </div>
 
             {
-                // 퀴즈 화면
-                !showDialogBox && (
-                    <div className={styles.top}>
-                        {/* 퀴즈 타이틀 */}
-                        <Quiz 
-                            term = {apiData.title}
-                        />
-                        {/* 호감도 */}
-                        <CrushBar />
-                    </div>
-                )
+            // 퀴즈 화면
+            !showDialogBox && (
+                <div className={styles.top}>
+                    {/* 퀴즈 타이틀 */}
+                    <Quiz 
+                        term = {apiData.title}
+                    />
+                    {/* 호감도 */}
+                    <CrushBar />
+                </div>
+            )
             }
         </div>
     );
