86c842a1a1faca16906bd6f921a817c6c870572b
An Jeongmin <95170874+102sae@users.noreply.github.com>
2023-10-17 13:11:35 +0900

    add : 실제 게임 창 추가

--- src/componets/StockGameNews.js
+++ src/componets/StockGameNews.js
@@ -1,65 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./StockGameNews.module.css";
+import StockGameNews from "../assets/images/StockGameNews.png";
+import NewsScenario from "../pages/Molly/NewsScenario";
 
-const StockGameNews = ({}) => {
-  return (
-    <div>
-      <svg
-        width="863"
-        height="624"
-        viewBox="0 0 863 624"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className={styles.news_wrap}
-      >
-        <g filter="url(#filter0_d_353_7464)">
-          <rect
-            width="855"
-            height="624"
-            transform="translate(4)"
-            fill="white"
-          />
-        </g>
-        <defs>
-          <filter
-            id="filter0_d_353_7464"
-            x="0"
-            y="0"
-            width="863"
-            height="632"
-            filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
-          >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
-            <feColorMatrix
-              in="SourceAlpha"
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-              result="hardAlpha"
-            />
-            <feOffset dy="4" />
-            <feGaussianBlur stdDeviation="2" />
-            <feComposite in2="hardAlpha" operator="out" />
-            <feColorMatrix
-              type="matrix"
-              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-            />
-            <feBlend
-              mode="normal"
-              in2="BackgroundImageFix"
-              result="effect1_dropShadow_353_7464"
-            />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="effect1_dropShadow_353_7464"
-              result="shape"
-            />
-          </filter>
-        </defs>
-      </svg>
+const StockGameNewsPaper = ({ showStockGameNews, toggleStockGameNews }) => {
+  const handleImageClick = () => {
+    toggleStockGameNews(!showStockGameNews);
+  };
+
+  return showStockGameNews ? (
+    <div className={styles.news_wrap} onClick={handleImageClick}>
+      <img
+        src={StockGameNews}
+        alt="StockGameNews"
+        className={styles.StockGameNews}
+      />
     </div>
-  );
+  ) : null;
 };
 
-export default StockGameNews;
+export default StockGameNewsPaper;
--- src/pages/Molly/Molly.js
+++ src/pages/Molly/Molly.js
@@ -10,7 +10,7 @@ import StockGameBox from "../../componets/StockGameBox";
 import { ReactComponent as HintButton } from "../../assets/images/hint_button.svg";
 import ProgressBar from "../../componets/ProgressBar";
 import TradingButton from "../../componets/TradingButton";
-import StockGameNews from "../../componets/StockGameNews";
+import StockGameNewsPaper from "../../componets/StockGameNews";
 import MollyTrading from "../../assets/images/Molly/molly_trading.png";
 import BubbleHint from "../../assets/images/Bubble/bubble_hint.svg";
 import BubbleProgress from "../../assets/images/Bubble/bubble_progress.svg";
@@ -18,6 +18,7 @@ import BubbleYear from "../../assets/images/Bubble/bubble_year.svg";
 import BubbleSell from "../../assets/images/Bubble/bubble_sell.svg";
 import BubbleBuy from "../../assets/images/Bubble/bubble_buy.svg";
 import BubbleStart from "../../assets/images/Bubble/bubble_start.svg";
+import BubbleStartMsg from "../../assets/images/Bubble/bubble_start_message.svg";
 import StockChart from "../../componets/StockChart";
 
 const Molly = () => {
@@ -32,6 +33,12 @@ const Molly = () => {
   const [showBubbleBuy, setShowBubbleBuy] = useState(false);
   const [showBubbleStart, setShowBubbleStart] = useState(false);
   const [showStockGameNews, setShowStockGameNews] = useState(false);
+  const [startStockGame, setStartStockGame] = useState(false);
+
+  //투자 게임 시작 버튼
+  const handleStartGame = () => {
+    setStartStockGame(true);
+  };
 
   // 다음 대화로 넘기기
   const handleDialogBoxClick = () => {
@@ -48,7 +55,7 @@ const Molly = () => {
   };
 
   const handleHintButtonClick = () => {
-    setShowStockGameNews(true);
+    setShowStockGameNews((prev) => !prev);
   };
 
   const handleKeyDown = (e) => {
@@ -95,7 +102,7 @@ const Molly = () => {
             )}
             {showDialogBox && (
               <div>
-                <div className={styles.character_wrap}>
+                <div className={styles.characterWrap}>
                   <img
                     src={MollyScenario[currentScenarioIndex].image}
                     alt="mollyImage"
@@ -148,28 +155,27 @@ const Molly = () => {
         )}
 
         {
-          /* 투자 설명 이후 투자 게임창 */
-          !showDialogBox && (
+          /* 게임 설명 이후 투자 게임창 */
+          !showDialogBox && !startStockGame && (
             <div>
               <div className={styles.top}>
                 <div
-                  className={styles.bubble_container}
+                  className={styles.bubbleContainer}
                   onMouseEnter={() => setShowBubbleHint(true)}
                   onMouseLeave={() => setShowBubbleHint(false)}
                 >
                   <HintButton onClick={handleHintButtonClick} />
                   {showBubbleHint && (
                     <img
-                      className={styles.bubble_hint}
+                      className={styles.bubbleHint}
                       src={BubbleHint}
                       alt="BubbleHint"
                     />
                   )}
-                  {/* {showStockGameNews && <StockGameNews />} */}
                 </div>
 
                 <div
-                  className={styles.bubble_container}
+                  className={styles.bubbleContainer}
                   onMouseEnter={() => setShowBubbleProgress(true)}
                   onMouseLeave={() => setShowBubbleProgress(false)}
                 >
@@ -178,7 +184,7 @@ const Molly = () => {
                   </div>
                   {showBubbleProgress && (
                     <img
-                      className={styles.bubble_progress}
+                      className={styles.bubbleProgress}
                       src={BubbleProgress}
                       alt="BubbleProgress"
                     />
@@ -189,7 +195,7 @@ const Molly = () => {
               </div>
               <div>
                 <div
-                  className={styles.bubble_container}
+                  className={styles.bubbleContainer}
                   onMouseEnter={() => {
                     setShowBubbleYear(true);
                     setShowBubbleSell(true);
@@ -204,21 +210,21 @@ const Molly = () => {
                   <TradingButton year={2020} />
                   {showBubbleYear && (
                     <img
-                      className={styles.bubble_year}
+                      className={styles.bubbleYear}
                       src={BubbleYear}
                       alt="BubbleYear"
                     />
                   )}
                   {showBubbleSell && (
                     <img
-                      className={styles.bubble_sell}
+                      className={styles.bubbleSell}
                       src={BubbleSell}
                       alt="BubbleSell"
                     />
                   )}
                   {showBubbleBuy && (
                     <img
-                      className={styles.bubble_buy}
+                      className={styles.bubbleBuy}
                       src={BubbleBuy}
                       alt="BubbleBuy"
                     />
@@ -243,7 +249,7 @@ const Molly = () => {
                   )}
                   {/* 몰리를 클릭하면 투자 게임 시작 */}
                   <img
-                    onClick={handleDialogBoxClick}
+                    onClick={handleStartGame}
                     className={styles.mollyImg}
                     src={MollyTrading}
                     alt="MollyTrading"
@@ -257,6 +263,65 @@ const Molly = () => {
           )
         }
       </div>
+      {
+        /* 실제 투자 게임 시작 */
+        startStockGame && (
+          <div>
+            <div className={styles.top}>
+              <HintButton onClick={handleHintButtonClick} />
+              {showStockGameNews && (
+                <StockGameNewsPaper
+                  showStockGameNews={showStockGameNews}
+                  toggleStockGameNews={handleHintButtonClick}
+                />
+              )}
+              <div
+                className={styles.bubbleContainer}
+                onMouseEnter={() => setShowBubbleProgress(true)}
+                onMouseLeave={() => setShowBubbleProgress(false)}
+              >
+                <div className={styles.progressWrap}>
+                  <ProgressBar character="몰리" progressCount={0} />
+                </div>
+              </div>
+              <CrushBar />
+            </div>
+            <div>
+              <div className={styles.bubbleContainer}>
+                <TradingButton year={2020} />
+              </div>
+
+              <div
+                className={styles.mollyImgWrap}
+                onMouseEnter={() => {
+                  setShowBubbleStart(true);
+                }}
+                onMouseLeave={() => {
+                  setShowBubbleStart(false);
+                }}
+              >
+                {showBubbleStart && (
+                  <img
+                    className={styles.bubbleStartMsg}
+                    src={BubbleStartMsg}
+                    alt="BubbleStartMsg"
+                  />
+                )}
+
+                <img
+                  onClick={handleDialogBoxClick}
+                  className={styles.mollyImg}
+                  src={MollyTrading}
+                  alt="MollyTrading"
+                />
+              </div>
+
+              <StockGameBox />
+              <StockChart />
+            </div>
+          </div>
+        )
+      }
     </div>
   );
 };
